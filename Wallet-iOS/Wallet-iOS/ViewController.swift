014f7cd6da23bdcbf80e8a94ed482d1dd4f34c1f
ttdp <matttian@hotmail.com>
2024-08-24 06:13:06 +0800

    initial commit

---
+++ Wallet-iOS/Wallet-iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Wallet-iOS
+//
+//  Created by Tian Tong on 2024/8/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
